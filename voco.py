062326f8f8dec671d8f456e9ae8734268f14d438
mahigithub1998 <52438347+mahigithub1998@users.noreply.github.com>
2019-07-02 15:19:21 +0530

    Update voco.py

--- voco.py
+++ voco.py
@@ -1,6 +1,6 @@
 ch=input()
 if(ch=='A' or ch=='a' or ch=='E' or ch=='e' or ch=='I' or ch=='i' or ch=='O' or ch=='o' or ch=='U' or ch=='u'):
-print("vowel")
+print(ch "vowel")
 else:
-print("consonent")
+print( ch "consonent")
    
