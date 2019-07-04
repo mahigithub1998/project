f627133875698ffcdfa86b28d6a0d94596e344c4
mahigithub1998 <52438347+mahigithub1998@users.noreply.github.com>
2019-07-04 12:12:32 +0530

    Update sumn.py

--- sumn.py
+++ sumn.py
@@ -1,9 +1,9 @@
-num=int(input())
-if num<0:
-  print("enter the positive number")
+nu=int(input())
+if nu<0:
+  print("enter positive number")
 else:
   sum=0
-  while(num>0):
-    sum=sum+num
-    num=num-1
+  while(nu>0):
+    sum=sum+nu
+    nu=nu-1
   print("the sum is",sum)
