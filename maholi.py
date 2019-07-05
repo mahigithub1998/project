a29176ccf774990ff8ace2682ec6a5dd01924e25
mahigithub1998 <52438347+mahigithub1998@users.noreply.github.com>
2019-07-05 11:47:21 +0530

    Create maholi.py

---
+++ maholi.py
@@ -0,0 +1,7 @@
+ma=input()
+if ((ma="Sunday") or (ma="Saturday")):
+  print("yes")
+elif ((ma="Monday") or (ma="Tuesay") or (ma="Wednesday") or (ma="Thursday") or (ma="Friday")):
+  print("no")
+else:
+  print("no")
