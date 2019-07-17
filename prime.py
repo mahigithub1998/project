6637174f87ff5b6bf50f96b1cd27f1ae30c977a9
mahigithub1998 <52438347+mahigithub1998@users.noreply.github.com>
2019-07-17 10:54:46 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,13 +1,14 @@
-m=int(input())
+a=int(input())
 b=2
 c=1
-while(b<w):
-    if(w%b==0):
-      c=0
-      break
-    else
-      b=b+1
+while(b<f):
+       if(f%b==0):
+              c=0
+              break
+       else:
+              b=b+1
 if c==0:
-    print('no')
+       print('no')
 else:
-    print('yes')
+       print('yes')
+
